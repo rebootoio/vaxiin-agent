2c3eccc992edcd017ff96a32c901d22d4df7ef30
Gai Radzi <gai@rebooto.io>
2022-03-16 18:07:11 +0200

    Fix restplus resources

--- app/routes/version.py
+++ app/routes/version.py
@@ -1,15 +1,10 @@
 from http import HTTPStatus
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource
 
 from version import VERSION
 
-ns = Namespace('Version', description='Get agent version')
 
-
-@ns.route('/')
 class Version(Resource):
 
-    @ns.doc('get agent version')
-    @ns.response(HTTPStatus.OK, 'Success')
     def get(self):
         return {"version": VERSION}, HTTPStatus.OK
